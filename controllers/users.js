0cab3ce23f883c1336608ade0509e888e008c2b2
IraGrafova <i.grafova@ntc-hitech.ru>
2023-08-29 13:15:34 +0300

    samesite

--- controllers/users.js
+++ controllers/users.js
@@ -53,7 +53,7 @@ const login = (req, res, next) => {
           res.cookie('jwt', jwt, {
             maxAge: 604800000,
             httpOnly: true,
-            sameSite: 'None',
+            sameSite: None,
             secure: NODE_ENV === 'production',
           });
           res.send({ data: user.toJSON() });
